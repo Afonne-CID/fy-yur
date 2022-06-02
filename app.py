c849c15936cb9caccdce6d3b1282d8ba9249a7b5
Afonne-CID <delightinbusiness@gmail.com>
2022-06-02 14:46:10 +0100

    finalizing

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from forms import *
 # from flask_script import Manager
 import collections
 collections.Callable = collections.abc.Callable
+import datetime
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -36,6 +37,41 @@ migrate = Migrate(app, db)
 # Models.
 #----------------------------------------------------------------------------#
 
+
+class Show(db.Model):
+    __tablename__ = 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)
+    venue_id = db.Column(db.Integer, db.ForeignKey(
+        'Venue.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey(
+        'Artist.id'), nullable=False)
+    # venue = db.relationship(
+    #     'Venue', backref=db.backref('venue_shows', cascade='all, delete'))
+    # artist = db.relationship(
+    #     'Artist', backref=db.backref('artist_shows', cascade='all, delete'))
+
+
+    def __repr__(self):
+        return f'<Show ID {self.id}: {self.name}>'
+
+
+    @property
+    def serialize(self):
+        return {'id': self.id,
+                'start_time': self.start_time.strftime("%m/%d/%Y, %H:%M:%S"),
+                'venue_id': self.venue_id,
+                'artist_id': self.artist_id
+                }
+
+    @property
+    def serialize_with_artist_venue(self):
+        return {'id': self.id,
+                'start_time': self.start_time.strftime("%m/%d/%Y, %H:%M:%S"),
+                'venue': [v for v in Venue.query.filter(Venue.id == self.venue_id).all()][0],
+                'artist': [a for a in Artist.query.filter(Artist.id == self.artist_id).all()][0]
+        }
+
 class Venue(db.Model):
     __tablename__ = 'Venue'
 
@@ -50,9 +86,9 @@ class Venue(db.Model):
     facebook_link = db.Column(db.String(120))
     website = db.Column(db.String(120))
     genres = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean)
+    seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
-    venue_shows = db.relationship('Show', back_populates='venue', lazy=True)
+    venue_shows = db.relationship('Show', backref='venue', lazy=True)
     
     def __repr__(self):
       return f'<Venue {self.id}: {self.name}>'
@@ -111,6 +147,22 @@ class Venue(db.Model):
                            for v in Venue.query.filter(Venue.city == self.city,
                                                        Venue.state == self.state).all()]}
 
+    @property
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'genres': self.genres.split(','),
+                'city': self.city,
+                'state': self.state,
+                'phone': self.phone,
+                'address': self.address,
+                'image_link': self.image_link,
+                'facebook_link': self.facebook_link,
+                'website': self.website,
+                'seeking_talent': self.seeking_talent,
+                'seeking_description': self.seeking_description
+                }
+
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
@@ -125,12 +177,26 @@ class Artist(db.Model):
     website = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
-    show = db.relationship('Show', backref='artist', lazy=True)
-    artist_shows = db.relationship('Show', back_populates='artist', lazy=True)
+    artist_show = db.relationship('Show', backref='artist', lazy=True)
 
     def __repr__(self):
       return f'<Artist {self.id}: {self.name}>'
 
+
+    @property
+    def serialize(self):
+        return {'id': self.id,
+                'name': self.name,
+                'genres': self.genres.split(','),
+                'city': self.city,
+                'state': self.state,
+                'phone': self.phone,
+                'image_link': self.image_link,
+                'facebook_link': self.facebook_link,
+                'website': self.website,
+                'seeking_venue': self.seeking_venue,
+                'seeking_description': self.seeking_description
+                }
 
     @property
     def serializer_with_show(self):
@@ -159,30 +225,6 @@ class Artist(db.Model):
                     Show.artist_id == self.id).all())
         }
 
-class Show(db.Model):
-    __tablename__ = 'Show'
-    id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime, nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey(
-        'Venue.id'), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey(
-        'Artist.id'), nullable=False)
-    # venue = db.relationship(
-    #     'Venue', backref=db.backref('venue_shows', cascade='all, delete'))
-    # artist = db.relationship(
-    #     'Artist', backref=db.backref('artist_shows', cascade='all, delete'))
-
-
-    def __repr__(self):
-        return f'<Show ID {self.id}: {self.name}>'
-
-    @property
-    def serialize_with_artist_venue(self):
-        return {'id': self.id,
-                'start_time': self.start_time.strftime("%m/%d/%Y, %H:%M:%S"),
-                'venue': [v for v in Venue.query.filter(Venue.id == self.venue_id).all()][0],
-                'artist': [a for a in Artist.query.filter(Artist.id == self.artist_id).all()][0]
-        }
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -223,7 +265,7 @@ def venues():
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
   search_term = request.form.get('search_term', '')
-  venues = Venue.query.fliter(Venue.name.ilike('%{}%'.format(search_term))).all()
+  venues = Venue.query.filter(Venue.name.ilike('%{}%'.format(search_term))).all()
   
   num_venues = len(venues)
 
@@ -240,7 +282,7 @@ def show_venue(venue_id):
   if not venue:
     abort(404)
 
-  data = venue.serializer_with_shows
+  data = venue.serializer_with_show
 
   return render_template('pages/show_venue.html', venue=data)
 
@@ -255,22 +297,24 @@ def create_venue_form():
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
 
+  form = VenueForm(request.form)
+
   obj = dict(request.form)
   obj['website'] = obj.pop('website_link')
   if obj['seeking_talent']:
     obj['seeking_talent'] = True
 
-  obj.genres = '.'.join(request.form.genres.data)
+  obj['genres'] = ','.join(form.genres.data)
 
   venue = Venue(**obj)
 
   try:
     db.session.add(venue)
     db.session.commit()
-    flash('Venue ' + request.form['name'] + ' was successfully listed! {}'.format(e))
+    flash('Venue ' + request.form['name'] + ' was successfully listed!')
   except Exception as e:
     db.session.rollback()
-    flash('An error occurred. Venue ' + venue.name + ' could not be listed.')
+    flash('An error occurred. Venue ' + venue.name + ' could not be listed. {}'.format(e))
   finally:
     db.session.close()
 
@@ -301,7 +345,7 @@ def delete_venue(venue_id):
 def artists():
   
   artists = Artist.query.all()
-  data = [artist.serializer_with_shows for artist in artists]
+  data = [artist.serializer_with_show for artist in artists]
 
   return render_template('pages/artists.html', artists=data)
 
@@ -324,10 +368,10 @@ def show_artist(artist_id):
 
   artist = Artist.query.get(artist_id)
 
-  if not data:
+  if not artist:
     abort(404)
   
-  data = artist.serializer_with_shows
+  data = artist.serializer_with_show
 
   return render_template('pages/show_artist.html', artist=data)
 
@@ -337,31 +381,41 @@ def show_artist(artist_id):
 #  ----------------------------------------------------------------
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-  artist = Artist.query.get(artist_id)
-  if artist:
-    form = ArtistForm(data=artist)
-  else:
+
+  update_artist = Artist.query.get(artist_id)
+
+  if not update_artist:
     abort(404)
 
+  artist = update_artist.serialize
+
+  form = ArtistForm(data=artist)
+  
   return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 @app.route('/artists/<int:artist_id>/edit', methods=['POST'])
 def edit_artist_submission(artist_id):
-  artist = Artist.query.get(artist_id)
-  obj = dict(request.form)
-  obj['website'] = obj.pop('website_link')
-
-  if obj['seeking_venue']:
-    obj['seeking_venue'] = True
-
-  for i in obj.keys():
-    artist[i] = obj[i]
-
-    artist.genres = '.'.join(request.form.genres.data)
-
-  db.session.commit()
+  form = ArtistForm(request.form)
+  try:
+      artist = Artist.query.get(artist_id)
+      artist.name = form.name.data,
+      artist.genres = json.dumps(form.genres.data),  # array json
+      artist.city = form.city.data,
+      artist.state = form.state.data,
+      artist.phone = form.phone.data,
+      artist.facebook_link = form.facebook_link.data,
+      artist.image_link = form.image_link.data,
+      
+      db.session.commit()
+      flash('Artist ' + request.form['name'] + ' was successfully updated!')
+  except Exception as e:
+      flash('An error occurred. Artist ' +
+            request.form['name'] + ' could not be updated.{}'.format(e))
+  finally:
+      db.session.close()
 
   return redirect(url_for('show_artist', artist_id=artist_id))
+
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
@@ -369,7 +423,8 @@ def edit_venue(venue_id):
   venue = Venue.query.get(venue_id)
 
   if venue:
-    form = VenueForm(data=venue)
+    update_venue = venue.serialize
+    form = VenueForm(data=update_venue)
   else:
     abort(404)
 
@@ -378,29 +433,28 @@ def edit_venue(venue_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['POST'])
 def edit_venue_submission(venue_id):
-  venue = Venue.query.get(venue_id)
-  obj = dict(request.form)
-  obj['website'] = obj.pop('website_link')
-
-  if obj['seeking_talent']:
-    obj['seeking_talent'] = True
-
+  form = VenueForm(request.form)
   try:
-    for i in obj.keys():
-      venue[i] = obj[i]
+      venue = Venue.query.get(venue_id)
+      venue.name = form.name.data,
+      venue.address = form.address.data,
+      venue.genres = '.'.join(form.genres.data),  # array json
+      venue.city = form.city.data,
+      venue.state = form.state.data,
+      venue.phone = form.phone.data,
+      venue.facebook_link = form.facebook_link.data,
+      venue.image_link = form.image_link.data,
       
-    venue.genres = '.'.join(request.form.genres.data)
-
-    db.session.commit()
-    flash('Venue ' + request.form['name'] + ' was successfully updated!')
+      db.session.commit()
+      # on successful db insert, flash success
+      flash('Venue ' + request.form['name'] + ' was successfully updated!')
   except Exception as e:
-    db.session.rollback()
-    flash('An error occurred. Venue ' +
-              request.form['name'] 
-              + ' could not be updated.{}'.format(e))
+      flash('An error occurred. Venue ' +
+            request.form['name'] + ' could not be updated.{}'.format(e))
+  finally:
+    db.session.close()
 
   return redirect(url_for('show_venue', venue_id=venue_id))
-
 
 
 #  ----------------------------------------------------------------
@@ -416,12 +470,13 @@ def create_artist_form():
 def create_artist_submission():
 
   obj = dict(request.form)
+  form = ArtistForm(request.form)
+
   obj['website'] = obj.pop('website_link')
-  if obj['seeking_venue']:
-    print('yes')
+  if obj['seeking_venue'] == 'y':
     obj['seeking_venue'] = True
 
-  artist.genres = '.'.join(request.form.genres.data) 
+  obj['genres'] = ','.join(form.genres.data) 
 
   artist = Artist(**obj)
 
@@ -447,7 +502,9 @@ def create_artist_submission():
 
 @app.route('/shows')
 def shows():
-  data = db.session.query(Show).join(Artist, Venue).all()
+  shows = Show.query.all()
+  data = [show.serialize_with_artist_venue for show in shows]
+
   return render_template('pages/shows.html', shows=data)
 
 
